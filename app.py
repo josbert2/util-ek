8170a764077fdf5dbdf9893983acca60bd6836de
josbert2 <joheandroid@gmail.com>
2021-11-15 16:55:18 -0300

    F

--- app.py
+++ app.py
@@ -57,7 +57,17 @@ def login_required(test):
 
 @app.route('/')
 def home():
-  
+    GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
+    CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.binary_location = GOOGLE_CHROME_PATH
+
+    browser = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+    browser.get("www.google.com"])
+
     return render_template('pages/placeholder.home.html')
 
 
